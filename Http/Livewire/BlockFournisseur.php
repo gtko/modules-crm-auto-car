6fc02710372c1a164f16fd3dcd6ab08ea448582c
aurelien3333 <aurelienverdeau@gmail.com>
2021-09-30 12:17:12 +0200

    wip block fournisseud add, validate, remove demande

--- Http/Livewire/BlockFournisseur.php
+++ Http/Livewire/BlockFournisseur.php
@@ -2,8 +2,11 @@
 
 namespace Modules\CrmAutoCar\Http\Livewire;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
+use Modules\CoreCRM\Contracts\Entities\DevisEntities;
+use Modules\CoreCRM\Contracts\Repositories\DevisRepositoryContract;
 use Modules\CoreCRM\Contracts\Repositories\FournisseurRepositoryContract;
 use Modules\CrmAutoCar\Mail\RequestFournisseurMail;
 
@@ -11,19 +14,62 @@ class BlockFournisseur extends Component
 {
     public $dossier;
     public $fournisseurs;
+    public $fournisseur_id;
+    public $devi_id;
+    public $prix;
+
+
+    protected $listeners = ['update' => '$refresh'];
+
+    protected $rules = [
+
+        'fournisseur_id' => 'required',
+        'devi_id' => 'required',
+        'prix' => 'required',
+
+    ];
 
     public function mount(FournisseurRepositoryContract $repFournisseur, $client, $dossier)
     {
         $this->dossier = $dossier;
         $this->fournisseurs = $repFournisseur->getAllList();
 
+
+
     }
 
-    public function send()
+    public function send(FournisseurRepositoryContract $repFournisseur, DevisRepositoryContract $repDevi)
     {
+        $this->validate();
+
+        $deviModel = $repDevi->fetchById($this->devi_id);
+        $fournisseurModel = $repFournisseur->fetchById($this->fournisseur_id);
 
         Mail::to(['d@gmail.com', 'test@test.com'])->send(new RequestFournisseurMail($this->dossier, 'test'));
 
+        $repDevi->sendPriceFournisseur($deviModel, $fournisseurModel, $this->prix, Carbon::now());
+
+        $this->emit('update');
+    }
+
+    public function validateDemande(int $devisId, int $fournisseurId, FournisseurRepositoryContract $repFournisseur, DevisRepositoryContract $repDevi)
+    {
+        $deviModel = $repDevi->fetchById($devisId);
+        $fournisseurModel = $repFournisseur->fetchById($fournisseurId);
+
+        $repDevi->validateFournisseur($deviModel, $fournisseurModel);
+
+        $this->emit('update');
+    }
+
+    public function delete(int $devisId, int $fournisseurId, FournisseurRepositoryContract $repFournisseur, DevisRepositoryContract $repDevi)
+    {
+        $deviModel = $repDevi->fetchById($devisId);
+        $fournisseurModel = $repFournisseur->fetchById($fournisseurId);
+
+        $repDevi->detachFournisseur($deviModel, $fournisseurModel);
+
+        $this->emit('update');
     }
 
     public function render()
