31b508531e74a1ab5778b4fca88f887ebf8d8ac2
aurelien3333 <aurelienverdeau@gmail.com>
2021-10-03 12:18:34 +0200

    wip block fournisseud add, validate, remove demande

--- Resources/views/livewire/block-fournisseur.blade.php
+++ Resources/views/livewire/block-fournisseur.blade.php
@@ -33,43 +33,42 @@
 
 
     <div>
-        <table class="divide-y divide-gray-200 w-full mt-4">
-            <thead class="bg-gray-200">
-            <tr>
-                <th scope="col" class="py-3  text-xs font-medium uppercase tracking-wider">
-                    Devis ID
-                </th>
-                <th scope="col" class="py-3  text-xs font-medium uppercase tracking-wider">
-                    Fournisseur
-                </th>
-                <th scope="col" class="py-3  text-xs font-medium uppercase tracking-wider">
-                    Prix
-                </th>
-                <th scope="col" class=" text-xs font-medium text-gray-500 uppercase tracking-wider">
 
-                </th>
+            <table class="divide-y divide-gray-200 w-full mt-4">
+                <thead class="bg-gray-200">
+                <tr>
+                    <th scope="col" class="py-3  text-xs font-medium uppercase tracking-wider">
+                        Devis ID
+                    </th>
+                    <th scope="col" class="py-3  text-xs font-medium uppercase tracking-wider">
+                        Fournisseur
+                    </th>
+                    <th scope="col" class="py-3  text-xs font-medium uppercase tracking-wider">
+                        Prix
+                    </th>
+                    <th scope="col" class=" text-xs font-medium text-gray-500 uppercase tracking-wider">
 
-            </tr>
-            </thead>
-            <tbody>
-            <!-- Odd row -->
+                    </th>
 
-            {{--            dd($this->dossier->devis->first()->fournisseurs->first()->pivot);--}}
+                </tr>
+                </thead>
+                <tbody>
+                <!-- Odd row -->
 
-            @foreach($dossier->devis as $devi)
-                @foreach($devi->fournisseurs as $fourni)
-                    <tr class="@if($fourni->pivot->validate) bg-green-500 @else bg-white  @endif">
-                        <td class="py-4 whitespace-nowrap text-sm font-medium text-center">
-                            {{ $devi->ref }}
-                        </td>
-                        <td class="py-4 text-sm text-center">
+                @foreach($dossier->devis as $devi)
+                    @foreach($devi->fournisseurs as $fourni)
+                        <tr class="@if($fourni->pivot->validate) bg-green-500 @else bg-white  @endif">
+                            <td class="py-4 whitespace-nowrap text-sm font-medium text-center">
+                                {{ $devi->ref }}
+                            </td>
+                            <td class="py-4 text-sm text-center">
 
-                            {{ $fourni->formatName }}
-                        </td>
-                        <td class="py-4 whitespace-nowrap text-sm text-center">
-                            {{ $fourni->pivot->prix }}
-                        </td>
-                        <td class="whitespace-nowrap text-sm text-right">
+                                {{ $fourni->formatName }}
+                            </td>
+                            <td class="py-4 whitespace-nowrap text-sm text-center">
+                                {{ $fourni->pivot->prix }}
+                            </td>
+                            <td class="whitespace-nowrap text-sm text-right">
                       <span class="flex flex-row justify-center">
                          <span
                              wire:click="delete({{ $devi->id }}, {{ $fourni->id }})"
@@ -83,13 +82,13 @@
                          >@icon('check', 20, 'mr-2')
                          </span>
                       </span>
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
+                    @endforeach
                 @endforeach
-            @endforeach
 
-            </tbody>
-        </table>
+                </tbody>
+            </table>
     </div>
 
 
