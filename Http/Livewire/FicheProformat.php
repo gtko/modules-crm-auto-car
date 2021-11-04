3243a2c78b3f996a42f54a66999183af20f40832
gtko <gtux.prog@gmail.com>
2021-11-04 19:57:00 +0100

    WIP flux

---
+++ Http/Livewire/FicheProformat.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\CrmAutoCar\Http\Livewire;
+
+use Livewire\Component;
+
+class FicheProformat extends Component
+{
+
+
+    public function render()
+    {
+        return view('crmautocar::livewire.fiche-proformat');
+    }
+}
