e6cdba9fe04ed9093e82c5d436a8dbda2870bd38
aurelien3333 <aurelienverdeau@gmail.com>
2021-11-16 15:54:49 +0100

    fiw btn valider devis

--- Resources/views/livewire/create-proformat.blade.php
+++ Resources/views/livewire/create-proformat.blade.php
@@ -1,3 +1,5 @@
 <div>
-    <button wire:click="createProFormat()">Valider le devis</button>
+    <button wire:click="createProFormat()" title="Valider le devis">
+        @icon('checkCircle', null, 'w-4 h-4 mr-1 mt-1')
+    </button>
 </div>
