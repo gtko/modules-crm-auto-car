9d95fa00a4bc44b1ba272415a4e6b0a2514d0871
aurelien3333 <aurelienverdeau@gmail.com>
2022-02-03 16:07:03 +0100

    add json dans dossier avec date et lieu depart arriver dans la cuve formulaire

--- Resources/views/livewire/element-list-cuve.blade.php
+++ Resources/views/livewire/element-list-cuve.blade.php
@@ -1,26 +1,23 @@
 <tr>
-    <td class="border-b dark:border-dark-5">
+    <td class="border-b dark:border-dark-5 text-xs">
+        <div class="flex items-center w-24">
+            <input class="form-check-input flex-none" type="checkbox" wire:model="selection" value="{{$dossier->id}}">
 
-        <input class="form-check-input flex-none" type="checkbox" wire:model="selection" value="{{$dossier->id}}">
-
-        <a href="{{route('dossiers.show', [$dossier->client, $dossier])}}"
-           class="w-6 h-6 flex-none image-fit relative ml-5">
-            <img alt="" class="rounded-full" src="{{$dossier->client->avatar_url}}">
-        </a>
-        <a href="{{route('dossiers.show', [$dossier->client, $dossier])}}"
-           class="inbox__item--sender truncate ml-3">{{$dossier->client->format_name}}</a>
+            <a href="{{route('dossiers.show', [$dossier->client, $dossier])}}"
+               class="inbox__item--sender ml-3">{{$dossier->client->format_name}}</a>
+        </div>
     </td>
-    <td class="border-b dark:border-dark-5">
+    <td class="border-b dark:border-dark-5 text-xs">
         <a href="{{route('dossiers.show', [$dossier->client, $dossier])}}">
             {{$dossier->source->label}}
         </a>
     </td>
-    <td class="border-b dark:border-dark-5">
+    <td class="border-b dark:border-dark-5 text-xs">
         <a href="{{route('dossiers.show', [$dossier->client, $dossier])}}">
             {{$dossier->client->email}}
         </a>
     </td>
-    <td class="border-b dark:border-dark-5">
+    <td class="border-b dark:border-dark-5 text-xs">
         @if($dossier->commercial->id != 1)
             {{$dossier->commercial->format_name}}
         @else
@@ -28,15 +25,27 @@
         @endif
 
     </td>
-    <td class="border-b dark:border-dark-5">
+    <td class="border-b dark:border-dark-5 text-xs">
         <a href="{{route('dossiers.show', [$dossier->client, $dossier])}}">
             {{$dossier->client->phone}}
         </a>
     </td>
-    <td class="border-b dark:border-dark-5">
+    <td class="border-b dark:border-dark-5 text-xs">
         {{$dossier->created_at->diffForHumans()}}
     </td>
-    <td>
+    <td class="border-b dark:border-dark-5 text-xs">
+        {{ $dossier->data['date_depart'] ?? 'N/A'}}
+    </td>
+    <td class="border-b dark:border-dark-5 text-xs">
+        {{ $dossier->data['lieu_depart'] ?? 'N/A'}}
+    </td>
+    <td class="border-b dark:border-dark-5 text-xs">
+        {{ $dossier->data['date_arrivee'] ?? 'N/A'}}
+    </td>
+    <td class="border-b dark:border-dark-5 text-xs">
+        {{ $dossier->data['lieu_arrivee'] ?? 'N/A'}}
+    </td>
+    <td class="border-b dark:border-dark-5 text-xs">
         @if ($filtre == 'corbeille')
             <span class="cursor-pointer" title="Restaurer" wire:click="restore({{$dossier->id}})">
                 @icon('save', null, 'mr-2')
