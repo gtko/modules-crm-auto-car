a6d0060146c1dce0cdce98785959c35d98333689
aurelien3333 <aurelienverdeau@gmail.com>
2022-02-11 10:48:09 +0100

    fix nom et ^prenom dans la card de la timeline validation devis

--- Resources/views/components/timeline/client/dossier/devis-externe/validation.blade.php
+++ Resources/views/components/timeline/client/dossier/devis-externe/validation.blade.php
@@ -19,7 +19,7 @@
             <div>
                 Avec les infos suivante :
                <ul>
-                   <li>Nom : <span class="text-yellow-600">{{ $flow->datas->getData()['name_validation'] ?? ''}}</span></li>
+                   <li>Nom : <span class="text-yellow-600">{{ ($flow->datas->getData()['nom_validation'] ?? '') . ' ' .  ($flow->datas->getData()['nom_validation'] ?? '')}}</span></li>
                    <li>Société : <span class="text-yellow-600">{{ $flow->datas->getData()['societe_validation'] ?? '' }}</span></li>
                    <li>Adresse : <span class="text-yellow-600">{{ $flow->datas->getData()['address_validation'] ?? ''}}</span></li>
                    <li>Type de paiment : <span class="text-yellow-600">{{ $flow->datas->getData()['paiement_type_validation'] ?? '' }}</span></li>
