121fee4e22b9514778fe11a3f9cc032be3627cf5
gtko <gtko@users.noreply.github.com>
2022-03-18 16:18:43 +0100

    Fix permissions

---
+++ Resources/views/cgv_html.blade.php
@@ -0,0 +1,18 @@
+
+<h1>CONDITIONS GENERALES DE LOCATION</h1>
+
+<h2>Dispositions générales</h2>
+
+<p>
+    La société CENTRALE AUTOCAR est une Société par actions simplifiées à associé unique, au capital social de  2 500 EUR,
+    immatriculée au RCS de Paris sous le numéro 853867703, dont le siège social est situé 57, Rue de Clisson – 75013 Paris (France).
+</p>
+
+<p>Son numéro de TVA intracommunautaire est le FR58853867703</p>
+
+<p>La société CENTRALE AUTOCAR a pour activité le transport public collectif occasionnel de personnes.</p>
+
+<p>
+    Plus précisément, elle propose un service de location d’autocars/minibus avec Chauffeur pour des trajets sur-mesure,
+    sur devis, à des particuliers et professionnels et une Mise en relation entre des sociétés de transport et chauffeurs d'autocar et/ou minibus avec des clients
+</p>
