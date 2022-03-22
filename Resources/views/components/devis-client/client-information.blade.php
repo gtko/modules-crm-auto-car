7a0348e0d867acd018056a21be2d5065a0c3c6d0
aurelien3333 <aurelienverdeau@gmail.com>
2022-03-22 11:50:26 +0100

    add civilite dans le devis

--- Resources/views/components/devis-client/client-information.blade.php
+++ Resources/views/components/devis-client/client-information.blade.php
@@ -2,7 +2,12 @@
     <h6 class="text-white bg-bleu text-3xl text-center py-2"
         style="font-family: 'Dancing Script', cursive;">A l'attention de</h6>
     <div class="bg-white border border-2 border-gray-400 p-4">
-        <span class="font-bold text-bleu text-base">{{ $devis->dossier->client->company ?? '' }}</span><br>
-        <span class="font-bold text-bleu text-base">{{ $devis->dossier->client->format_name ?? '' }}</span>
+        <span class="font-bold text-bleu text-base">
+            {{ $devis->dossier->client->company ?? '' }}
+        </span><br>
+        <span class="font-bold text-bleu text-base">
+              @if($devis->dossier->client->personne->gender === 'male')
+                Monsieur @elseif($devis->dossier->client->personne->gender === 'female') Madame @endif
+            {{ $devis->dossier->client->format_name ?? '' }}</span>
     </div>
 </div>
