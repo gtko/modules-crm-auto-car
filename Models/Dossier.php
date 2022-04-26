dfb911e814ad373d3e44bd670c28176f6cd0c41d
gtko <gtko@users.noreply.github.com>
2022-04-26 11:03:24 +0200

    oublie dossier

--- Models/Dossier.php
+++ Models/Dossier.php
@@ -4,14 +4,29 @@ namespace Modules\CrmAutoCar\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Modules\TaskCalendarCRM\Models\Task;
 
 class Dossier extends \Modules\CoreCRM\Models\Dossier
 {
 
     public $with = ['client', 'commercial'];
 
+    public function getMorphClass()
+    {
+        return \Modules\CoreCRM\Models\Dossier::class;
+    }
+
     public function contactFournisseurs():HasMany
     {
         return $this->hasMany(ContactFournisseur::class);
     }
+
+    public function tasks():MorphMany
+    {
+        return $this->morphMany(Task::class, 'taskable');
+    }
+
 }
