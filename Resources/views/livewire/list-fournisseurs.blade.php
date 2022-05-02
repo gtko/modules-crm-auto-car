4773eb69afc3cf338fa56ed83b5c1a9d16fa27cb
gtko <gtko@users.noreply.github.com>
2022-05-02 19:22:48 +0200

    Refonte utilisateurs

--- Resources/views/livewire/list-fournisseurs.blade.php
+++ Resources/views/livewire/list-fournisseurs.blade.php
@@ -1,4 +1,4 @@
-<div class="mt-5">
+<div class="mt-5 relative">
 
     <div wire:loading class="border border-b-0 border-r-0 absolute top-0 w-full left-0 h-14 flex items-center justify-center z-40 bg-gray-50 text-gray-800 rounded p-2">
         <div class="flex justify-start items-center w-full h-full">
