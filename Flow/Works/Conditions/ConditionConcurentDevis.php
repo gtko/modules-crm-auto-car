825d44f47c8f0345aef30604ed38c42f25e8dac3
gtko <gtux.prog@gmail.com>
2022-08-18 14:57:31 +0200

    searchable worflow log

--- Flow/Works/Conditions/ConditionConcurentDevis.php
+++ Flow/Works/Conditions/ConditionConcurentDevis.php
@@ -5,6 +5,7 @@ namespace Modules\CrmAutoCar\Flow\Works\Conditions;
 use Carbon\Carbon;
 use Modules\CoreCRM\Flow\Works\Params\ParamsBoolean;
 use Modules\CoreCRM\Flow\Works\Params\WorkFlowParams;
+use Modules\CoreCRM\Flow\Works\WorkflowLog;
 use Modules\CrmAutoCar\Flow\Works\Params\ParamsBrand;
 use Modules\CrmAutoCar\Flow\Works\Params\ParamsNombreJours;
 
@@ -21,8 +22,9 @@ class ConditionConcurentDevis extends \Modules\CoreCRM\Flow\Works\Conditions\Wor
         $data = $this->event->getData();
         $trajets = collect($data['devis']->data['trajets'][0]);
 
-        //on cherche le plus petit nombre de jours
         $total = $trajets['brands'][$this->getValueTarget()] ?? 0;
+
+        app(WorkflowLog::class)->SetSubMessage("Prix de la brand ".$this->getValueTarget()." " . print_r($total, true));
 
         if($total > 0) return $this->getValueTarget();
 
